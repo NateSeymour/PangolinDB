a371d4830c32f9fb19ebf49e79ecb8c5809f2fc1
Nathan Seymour <nathan@seymour.global>
2023-04-10 14:10:00 -0400

    feat(schema): inital strutures

--- src/Schema.h
+++ src/Schema.h
@@ -1,5 +1,25 @@
-
 #ifndef PDB_SCHEMA_H
 #define PDB_SCHEMA_H
+
+#include <string>
+#include <map>
+#include <cstdlib>
+
+namespace pdb
+{
+    struct Member
+    {
+        std::size_t size;
+        std::size_t location;
+        bool is_numeric;
+        bool is_signed;
+        bool is_unique;
+    };
+
+    struct Schema
+    {
+        std::map<std::string, Member> members;
+    };
+}
 
 #endif //PDB_SCHEMA_H
